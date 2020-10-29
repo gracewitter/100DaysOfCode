3a21c17c140edfa10cc0d3b3c89914917b2eec25
Grace Witter <grace@happyporch.com>
2020-10-29 22:47:52 +0000

    3d animation and codecademy

---
+++ practice exercises/codecademy/animal fun facts/app.js
@@ -0,0 +1,33 @@
+import { animals } from "./animals";
+import React from "react";
+import ReactDOM from "react-dom";
+
+const title = "";
+const background = (
+  <img class="background" alt="ocean" src="/images/ocean.jpg" />
+);
+
+const images = [];
+for (const animal in animals) {
+  images.push(
+    <img
+      key={animal}
+      className="animal"
+      alt={animal}
+      src={animals[animal].image}
+      aria-label={animal}
+      role="button"
+    />
+  );
+}
+
+const animalFacts = (
+  <div>
+    <h1>{title ? title : "Click an animal for a fun fact"}</h1>
+    {background}
+    <div className="animals">{images}</div>
+    <p id="fact"></p>
+  </div>
+);
+
+ReactDOM.render(animalFacts, document.getElementById("root"));
